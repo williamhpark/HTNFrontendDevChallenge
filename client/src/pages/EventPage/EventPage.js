c8923d0fe67f4807675bb0300897b5164ea62b1f
William Park <william.h.park@gmail.com>
2021-03-15 22:40:28 -0700

    Minor comment addition

--- client/src/pages/EventPage/EventPage.js
+++ client/src/pages/EventPage/EventPage.js
@@ -27,6 +27,7 @@ const EventPage = (props) => {
     fetchData();
   }, []);
 
+  // Checks if the inputted object is empty
   const isObjectEmpty = (obj) => {
     return Object.keys(obj).length === 0 && obj.constructor === Object;
   };
